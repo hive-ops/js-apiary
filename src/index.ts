d52cd9daf06213f168901fb8406852e9f2042799
Adeniyi Bello <poph2@users.noreply.github.com>
2024-08-01 10:40:30 -0400

    Added GitHub action and some documentation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-import { createPromiseClient } from "@connectrpc/connect";
+import { createPromiseClient, PromiseClient } from "@connectrpc/connect";
 import { createGrpcTransport } from "@connectrpc/connect-node";
 import { ApiaryService } from "./pb/apiary/v1/service_connect";
 
+/**
+ * This module provides a client for the ApiaryService.
+ * @module
+ */
+
+/**
+ * Configuration for the client.
+ */
 export type ClientConfig = {
     baseUrl: string;
 };
 
-export const getClient = (config: ClientConfig) => {
+/**
+ * Create a client for the ApiaryService.
+ * @param config
+ * @returns A client for the ApiaryService.
+ */
+export const getClient = (config: ClientConfig): PromiseClient<typeof ApiaryService> => {
     const transport = createGrpcTransport({
         baseUrl: config.baseUrl,
         httpVersion: "2",
     });
     return createPromiseClient(ApiaryService, transport);
 };
-
